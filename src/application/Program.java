3948c9b8bae2d6d7a98a428e432e262f048b83d6
Luiz Felipe <luizfelipe01.2007@gmail.com>
2025-11-17 00:38:49 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,19 +2,14 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
-
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.of(2025, 11, 21), 3000.0, obj);
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println(obj);
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
     }
 }

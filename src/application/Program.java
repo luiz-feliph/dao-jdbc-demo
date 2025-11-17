5f581d5bdb32cd77893400352a60f66d7e925c6e
Luiz Felipe <luizfelipe01.2007@gmail.com>
2025-11-16 23:49:13 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,17 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.of(2025, 11, 21), 3000.0, obj);
+
         System.out.println(obj);
+        System.out.println(seller);
     }
 }

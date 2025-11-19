030f5b310af2cc21bef9ca0196221865ef616c3d
Luiz Felipe <luizfelipe01.2007@gmail.com>
2025-11-18 23:55:30 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class Program {
@@ -27,6 +28,11 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 4: seller insert =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.of(2001, 6, 14), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
 
     }
 }

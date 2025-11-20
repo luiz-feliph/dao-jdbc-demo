28dda98bfb5be1a1d3493afb555bda99a38a4a69
Luiz Felipe <luizfelipe01.2007@gmail.com>
2025-11-20 01:49:26 -0300

    Department delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -4,9 +4,11 @@ import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
         System.out.println("=== TEST 1: department findById =====");
@@ -29,5 +31,13 @@ public class Program2 {
         department.setName("Games");
         departmentDao.update(department);
         System.out.println("Update completed!");
+
+        System.out.println("\n=== TEST 5: department delete =====");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Delete completed!");
+
+        sc.close();
     }
 }

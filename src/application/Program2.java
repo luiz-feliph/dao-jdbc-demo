4cfd105cd667076afa117ae283fb5d6778de4f39
Luiz Felipe <luizfelipe01.2007@gmail.com>
2025-11-20 01:32:50 -0300

    Department insert implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -3,8 +3,6 @@ package application;
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
-import model.entities.Seller;
-
 import java.util.List;
 
 public class Program2 {
@@ -20,5 +18,10 @@ public class Program2 {
         for (Department obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 3: Department insert =====");
+        Department newDepartment = new Department(null, "Foods");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id = " + newDepartment.getId());
     }
 }
